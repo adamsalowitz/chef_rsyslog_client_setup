761b7385c43d965eb5a47831586a7dbc9e135597
Wu Tang Financial <wutangfinacial@detwa.com>
2019-09-03 22:34:44 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_rsyslog_client_setup'
+maintainer       'Adam Salowitz'
+maintainer_email 'adam.salowitz+github@gmail.com'
+license          'CC0'
+description      'Installs/Configures chef_rsyslog_client_setup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
